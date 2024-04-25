6858ed35366ba571c6e71199544ea4f766e1be7a
Itor Isaias da Silva <itor.isaias@gmail.com>
2024-04-25 10:17:29 -0300

    Update 1036.c

--- beecrowd/1036.c
+++ beecrowd/1036.c
@@ -10,3 +10,22 @@
 
 // Saída
 // Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+#include <stdio.h>
+#include <math.h>
+int main(){
+  double A, B, C, delta, r1, r2;
+  scanf("%lf %lf %lf", &A, &B, &C);
+
+  // (-b±√(b²-4ac))/(2a)
+  delta = (B*B) - (4*A*C);
+  r1 = ((-B) + (sqrt(delta))) / (2*A);
+  r2 = ((-B) - (sqrt(delta))) / (2*A);
+
+  if (A <= 0 || B <= 0 || C <= 0 || delta <= 0) {
+    printf("Impossivel calcular\n");
+  } else {
+    printf("R1 = %.5lf\n", r1);
+    printf("R2 = %.5lf\n", r2);
+  }
+  return 0;
+}
