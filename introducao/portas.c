46ff6f3586f1aa9989fea4d6506401e8a6c8cb7e
Itor Isaias <itor.isaias@gmail.com>
2024-08-31 09:27:53 -0300

    adicionando comentarios

--- introducao/portas.c
+++ introducao/portas.c
@@ -11,16 +11,20 @@ int main()
     while (TRUE)
     {
         scanf("%d %d", &A, &N);
+        // condicao de parada
         if (A == 0 && N == 0)
             break;
+        // inicializa o predio
         for (c = 0; c < A; c++)
             predio[c] = FALSE;
+        // leitura dos numeros
         for (c = 0; c < N; c++)
         {
             scanf("%d", &numero);
             for (x = numero - 1; x < A; x += numero)
                 predio[x] = !predio[x];
         }
+        // impressao do predio
         for (c = 0; c < A; c++)
             printf("%c", predio[c] ? 'O' : 'C');
         printf("\n");
